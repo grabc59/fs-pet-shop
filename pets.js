9adaf15bc07006d78d7e69059a073bab3f58dabe
Christopher Grabski <grabc59@gmail.com>
2016-11-08 18:41:44 -0700

    adding out of bounds handling for read function

--- pets.js
+++ pets.js
@@ -1,13 +1,32 @@
 'use strict';
+
 var fs = require('fs');
 var path = require('path');
-var guestsPath = path.join(__dirname, 'guests.json');
+
+var petsPath = path.join(__dirname, 'pets.json');
+
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
 if (cmd === 'read') {
-
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    var pets = JSON.parse(data);
+    // console.log(process.argv[3]);
+    if (Number(process.argv[3]) >= 0 && Number(process.argv[3]) <= pets.length - 1) {
+      var index = process.argv[3];
+      var pet = pets[index];
+      console.log(pet);
+    } else if (!process.argv[3]) {
+      console.log(pets);
+    } else {
+      console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+      process.exit(1);
+    }
+  });
 } else if (cmd === 'create') {
 
 } else if (cmd === 'update') {
@@ -18,10 +37,3 @@ if (cmd === 'read') {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
   process.exit(1);
 }
-
-// ```shell
-// $ node pets.js
-// Usage: node pets.js [read | create | update | destroy]
-// ```
-// console.error(`Usage: ${node} ${file} ${cmd} GUEST`);
-// process.exit(1);
