6fb9af1f2b8f0f31704caeb61516737403d74f4f
Christopher Grabski <grabc59@gmail.com>
2016-11-09 21:27:45 -0700

    server respondin to requests and rejecting invalid indexes in the URL

--- expressServer.js
+++ expressServer.js
@@ -1,17 +1,48 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
-
-app.use(function(req, res) {
-  res.send('Hello World');
-});
+const petRegExp = /^\/pets\/(.*)$/;
 
 app.get('/pets', function(req, res) {
-  res.send([{ "age": 7, "kind": "rainbow", "name": "fido" }, { "age": 5, "kind": "snake", "name": "Buttons" }])
-})
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    var pets = JSON.parse(petsJSON);
+    res.set('Content-Type', 'application/json');
+    res.send(pets);
+  });
+});
+
+app.get('/pets/:index', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    var index = Number.parseInt(req.params.index);
+    var pets = JSON.parse(petsJSON);
+    if (index < 0 || index >= pets.length || Number.isNaN(index)) {
+      return res.sendStatus(404);
+    }
+    var pet = pets[index];
+    res.set('Content-Type', 'application/json');
+    res.send(pet);
+  });
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
 
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
+module.exports = app;
